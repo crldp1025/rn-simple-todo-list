dd2523a947e92435ef62bd577d7d3b59bccfe433
Carlo <crldp1025@gmail.com>
2024-03-02 01:10:28 +0800

    React Navigation Installation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    'semi': 0,
+    'eol-last': 0,
+    'comma-dangle': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'react/no-unstable-nested-components': 0,
+    'react/self-closing-comp': 0,
+    'react-native/no-inline-styles': 0,
+    'no-trailing-spaces': 0,
+    'quotes': 0,
+  }
 };
---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@rneui/base'
+import React from 'react'
+import { SafeAreaView, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Homescreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
